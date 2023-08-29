2b1c8cd5761568e4d4040c4c8bfc9765dadb4089
Qianxy <xyqian67@gmail.com>
2023-08-28 23:38:10 -0700

    Cleanup vm code

--- src/vm.rs
+++ src/vm.rs
@@ -8,11 +8,11 @@ struct VM {
 
     stat: Stat,
 
-    pc: usize,
+    pc: u64,
     mem: Vec<u8>,
 }
 
-#[derive(Default)]
+#[derive(Default, PartialEq, Eq)]
 enum Stat {
     /// Normal operation
     #[default]
@@ -50,31 +50,70 @@ impl VM {
     }
 
     fn step(&mut self) {
-        macro_rules! read {
+        macro_rules! oob {
+            () => {{
+                self.stat = Stat::Adr;
+                return;
+            }};
+        }
+
+        macro_rules! advance {
             () => {
-                if let Some(b) = self.mem.get(self.pc) {
+                if let Some(b) = self.mem.get(self.pc as usize) {
                     self.pc += 1;
                     ((b >> 4) as usize, (b & 0xF) as usize)
                 } else {
-                    self.stat = Stat::Adr;
-                    return;
+                    oob!()
                 }
             };
         }
 
         macro_rules! read8 {
-            () => {
-                if let Some(bs) = self.mem.get(self.pc..self.pc + 8) {
-                    self.pc += 8;
-                    u64::from_le_bytes(bs.try_into().unwrap())
+            ($addr:expr) => {
+                if let Some(m) = self.mem.get($addr as usize..$addr as usize + 8) {
+                    u64::from_le_bytes(m.try_into().unwrap())
                 } else {
-                    self.stat = Stat::Adr;
-                    return;
+                    oob!()
                 }
             };
         }
 
-        let (hi, lo) = read!();
+        macro_rules! write8 {
+            ($addr:expr, $v:expr) => {
+                if let Some(m) = self.mem.get_mut($addr as usize..$addr as usize + 8) {
+                    m.copy_from_slice(&$v.to_le_bytes());
+                } else {
+                    oob!()
+                }
+            };
+        }
+
+        macro_rules! advance8 {
+            () => {{
+                let v = read8!(self.pc);
+                self.pc += 8;
+
+                v
+            }};
+        }
+
+        macro_rules! push {
+            ($v:expr) => {{
+                let rsp = &mut self.registers[4];
+                write8!(*rsp - 8, $v);
+                *rsp -= 8;
+            }};
+        }
+
+        macro_rules! pop {
+            () => {{
+                let rsp = &mut self.registers[4];
+                *rsp += 8;
+                read8!(*rsp - 8)
+            }};
+        }
+
+        let (hi, lo) = advance!();
 
         match (hi, lo) {
             // halt
@@ -85,7 +124,7 @@ impl VM {
 
             // rrmovq, cmovXX
             (0x2, 0x0..=0x6) => {
-                let (ra, rb) = read!();
+                let (ra, rb) = advance!();
                 if self.branch(lo) {
                     self.registers[rb] = self.registers[ra];
                 }
@@ -93,38 +132,30 @@ impl VM {
 
             // irmovq
             (0x3, 0x0) => {
-                let (_, rb) = read!();
-                let v = read8!();
+                let (_, rb) = advance!();
+                let v = advance8!();
                 self.registers[rb] = v;
             }
 
             // rmmovq
             (0x4, 0x0) => {
-                let (ra, rb) = read!();
-                let d = read8!();
-                let addr = (self.registers[rb] + d) as usize;
-                if let Some(m) = self.mem.get_mut(addr..addr + 8) {
-                    m.copy_from_slice(&self.registers[ra].to_le_bytes());
-                } else {
-                    self.stat = Stat::Adr;
-                }
+                let (ra, rb) = advance!();
+                let d = advance8!();
+                let addr = self.registers[rb] + d;
+                write8!(addr, self.registers[ra]);
             }
 
             // mrmovq
             (0x5, 0x0) => {
-                let (ra, rb) = read!();
-                let d = read8!();
-                let addr = (self.registers[rb] + d) as usize;
-                if let Some(m) = self.mem.get(addr..addr + 8) {
-                    self.registers[ra] = u64::from_le_bytes(m.try_into().unwrap());
-                } else {
-                    self.stat = Stat::Adr;
-                }
+                let (ra, rb) = advance!();
+                let d = advance8!();
+                let addr = self.registers[rb] + d;
+                self.registers[ra] = read8!(addr);
             }
 
             // OPq
             (0x6, 0x0..=0x3) => {
-                let (ra, rb) = read!();
+                let (ra, rb) = advance!();
                 let a = self.registers[ra];
                 let b = self.registers[rb];
                 let (result, overflow) = match lo {
@@ -143,61 +174,42 @@ impl VM {
 
             // jXX
             (0x7, 0x0..=0x6) => {
-                let dest = read8!();
+                let dest = advance8!();
                 if self.branch(lo) {
-                    self.pc = dest as usize;
+                    self.pc = dest;
                 }
             }
 
             // call
             (0x8, 0x0) => {
-                let dest = read8!();
-                let rsp = self.registers[4] as usize;
-                if let Some(m) = self.mem.get_mut(rsp - 8..rsp) {
-                    m.copy_from_slice(&self.pc.to_le_bytes());
-                    self.registers[4] -= 8;
-                    self.pc = dest as usize;
-                } else {
-                    self.stat = Stat::Adr;
-                }
+                let dest = advance8!();
+                push!(self.pc);
+                self.pc = dest;
             }
 
             // ret
-            (0x9, 0x0) => {
-                self.registers[4] += 8;
-                let rsp = self.registers[4] as usize;
-                if let Some(m) = self.mem.get(rsp - 8..rsp) {
-                    self.pc = usize::from_le_bytes(m.try_into().unwrap());
-                } else {
-                    self.stat = Stat::Adr;
-                }
-            }
+            (0x9, 0x0) => self.pc = pop!(),
 
             // pushq
             (0xA, 0x0) => {
-                let (ra, _) = read!();
-                let rsp = self.registers[4] as usize;
-                if let Some(m) = self.mem.get_mut(rsp - 8..rsp) {
-                    m.copy_from_slice(&self.registers[ra].to_le_bytes());
-                    self.registers[4] -= 8;
-                } else {
-                    self.stat = Stat::Adr;
-                }
+                let (ra, _) = advance!();
+                let v = self.registers[ra];
+                push!(v);
             }
 
             // popq
             (0xB, 0x0) => {
-                let (ra, _) = read!();
-                self.registers[4] += 8;
-                let rsp = self.registers[4] as usize;
-                if let Some(m) = self.mem.get(rsp - 8..rsp) {
-                    self.registers[ra] = u64::from_le_bytes(m.try_into().unwrap());
-                } else {
-                    self.stat = Stat::Adr;
-                }
+                let (ra, _) = advance!();
+                self.registers[ra] = pop!();
             }
 
             _ => self.stat = Stat::Ins,
+        }
+    }
+
+    fn run(&mut self) {
+        while self.stat == Stat::Aok {
+            self.step();
         }
     }
 }
